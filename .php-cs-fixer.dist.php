f292becc9539605ea9d28bcf7a4fefa48112e0af
Anar Haqverdiyev <haqverdiyevanar@gmail.com>
2026-01-14 16:50:31 +0100

    chore: Configure PHP quality tools

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__.'/src')
+    ->exclude('vendor')
+    ->exclude('var')
+    ->exclude('public/bundles');
+
+return (new Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@PHP81Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'single_line_throw' => false,
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'extra',
+                'throw',
+                'use',
+                'use_trait',
+            ],
+        ],
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+    ->setLineEnding("\n");
